1a8b24b0e674e785ca84a01e67c9ef37684bbc92
Krutte <s236656@stud.hioa.no>
2016-11-12 13:31:15 +0100

    Minor dropdown test in BankIndex.

--- ProsjektoppgaveNettbank/Scripts/Custom.js
+++ ProsjektoppgaveNettbank/Scripts/Custom.js
@@ -1,9 +1,1 @@
-﻿$(function () {
-    // Setup drop down menu
-    $('.dropdown-toggle').dropdown();
-
-    // Fix input element click problem
-    $('.dropdown input, .dropdown label').click(function (e) {
-        e.stopPropagation();
-    });
-});
+﻿
